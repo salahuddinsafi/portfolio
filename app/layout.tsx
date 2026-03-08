873e879aeae8483ae55b99a8a76ed6db569fe339
Salahuddin Safi <zaydsafi9@gmail.com>
2026-03-08 21:03:11 +0430

    yt

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Provider from "@/components/Hoc/Provider";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import { Analytics } from "@vercel/analytics/next";
 import Footer from "@/components/Home/Footer/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${font.className} antialiased`}>
       <Analytics/>
+      <SpeedInsights/>
         <Provider>
           <ResponsiveNav />
           {children}
